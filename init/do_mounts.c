948c0c54f06d4a6395fb20bb8e5f57b37816f362
Joern Engel <joern@logfs.org>
2012-07-12 10:59:38 -0700

    add blockconsole version 1.1
    Console driver similar to netconsole, except it writes to a block
    device.  Can be useful in a setup where netconsole, for whatever
    reasons, is impractical.
    
    Changes since version 1.0:
    - Header format overhaul, addressing several annoyances when actually
      using blockconsole for production.
    - Steve Hodgson added a panic notifier.
    - Added improvements from Borislav Petkov.
    
    Signed-off-by: Steve Hodgson <steve@purestorage.com>
    Signed-off-by: Joern Engel <joern@logfs.org>

--- init/do_mounts.c
+++ init/do_mounts.c
@@ -106,7 +106,7 @@ no_match:
  *
  * Returns the matching dev_t on success or 0 on failure.
  */
-static dev_t devt_from_partuuid(char *uuid_str)
+static dev_t devt_from_partuuid(const char *uuid_str)
 {
 	dev_t res = 0;
 	struct device *dev = NULL;
@@ -183,7 +183,7 @@ done:
  *	bangs.
  */
 
-dev_t name_to_dev_t(char *name)
+dev_t name_to_dev_t(const char *name)
 {
 	char s[32];
 	char *p;
